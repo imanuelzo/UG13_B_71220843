eb9a90708708426cbc02ab3b3e1663850a4e4d18
imanuelzo <117885348+imanuelzo@users.noreply.github.com>
2022-12-15 13:02:14 +0700

    Add files via upload

---
+++ 1_B_71220843.py
@@ -0,0 +1,36 @@
+import turtle
+
+s = turtle.Screen()
+
+#E
+e = turtle.Turtle()
+e.left(90)
+e.forward(200)
+e.right(90)
+e.forward(70)
+e.right(90)
+e.forward(20)
+e.right(90)
+e.forward(55)
+e.left(90)
+e.forward(20)
+e.left(90)
+e.forward(55)
+e.right(90)
+e.forward(20)
+e.right(90)
+e.forward(55)
+e.left(90)
+e.forward(20)
+e.left(90)
+e.forward(55)
+e.right(90)
+e.forward(20)
+e.right(90)
+e.forward(70)
+
+
+
+
+s.exitonclick()
+
